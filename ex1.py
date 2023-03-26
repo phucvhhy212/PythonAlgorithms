1faa15c9bbbfa40868d2358b42eabe57dc205a4e
phucvhhy212 <phucvhhy212@gmail.com>
2023-03-26 22:51:46 +0700

    first

---
+++ ex1.py
@@ -0,0 +1,15 @@
+def MaxDuplicateCount(input):
+    count = 1
+    maxCount = 1
+    for x in range(len(input)-1):
+        if input[x] == input[x+1]:
+            count += 1
+        else:
+            count = 1
+        if maxCount < count:
+            maxCount = count
+    
+    return maxCount
+
+
+print(MaxDuplicateCount("aaabbbdeefdddd"))  
